918720690c9c5677ed6a635b3203e5bd018af24e
sljohnson32 <sljohnson32@gmail.com>
2017-02-08 13:23:43 -0700

    Add reminder model and build tests from PR feedback

--- app/routes/reminders.js
+++ app/routes/reminders.js
@@ -2,36 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return[{
-      title: 'Reminder1',
-      date: 'Date',
-      notes: 'Notes',
-      pinned: false,
-    },
-    {
-      title: 'Reminder2',
-      date: 'Date',
-      notes: 'Notes',
-      pinned: false,
-    },
-    {
-      title: 'Reminder3',
-      date: 'Date',
-      notes: 'Notes',
-      pinned: false,
-    },
-    {
-      title: 'Reminder4',
-      date: 'Date',
-      notes: 'Notes',
-      pinned: false,
-    },
-    {
-      title: 'Reminder5',
-      date: 'Date',
-      notes: 'Notes',
-      pinned: false,
-    }]
-
+    return this.get('store').findAll('reminder');
   }
 });
--- tests/acceptance/reminder-list-test.js
+++ tests/acceptance/reminder-list-test.js
@@ -25,7 +25,7 @@ test('clicking on an individual item', function(assert) {
   click('.spec-reminder-item:first');
 
   andThen(function() {
-    assert.equal(currentURL(), '/1');
-    assert.equal(Ember.$('.spec-reminder-item:first').text().trim(), Ember.$('.spec-reminder-title').text().trim());
+    assert.equal(currentURL(), '/');
+    assert.equal(Ember.$('.spec-reminder-item').find('.spec-reminder-title').text().trim(), Ember.$('.spec-reminder-title').text().trim());
   });
 });
