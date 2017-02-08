70e6c30c2198b78ed904fff90510bf4e8899248b
sljohnson32 <sljohnson32@gmail.com>
2017-02-07 17:22:18 -0700

    Display reminders on homepage

---
+++ app/routes/reminders.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return[{
+      title: 'Reminder1',
+      date: 'Date',
+      notes: 'Notes',
+      pinned: false,
+    },
+    {
+      title: 'Reminder2',
+      date: 'Date',
+      notes: 'Notes',
+      pinned: false,
+    },
+    {
+      title: 'Reminder3',
+      date: 'Date',
+      notes: 'Notes',
+      pinned: false,
+    }]
+
+  }
+});
