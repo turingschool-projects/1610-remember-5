30c0b17c1670071c099ce8ef439be9d26473875e
mlimberg <limbergmike@gmail.com>
2017-02-09 13:30:32 -0700

    Correct issues from the original PR

--- app/routes/reminders/reminder.js
+++ app/routes/reminders/reminder.js
@@ -1,10 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  
 
-  model() {
-    // const reminders = this.get('store').findAll('reminder')
-    // return this.get('store').findAll(reminder => reminder.id === params.reminder_id)
+  model(params) {
+    return this.get('store').peekRecord('reminder', params.reminder_id)
   }
 });
