da4c441dfb7f5679f150a99d866987b057fdb644
mlimberg <limbergmike@gmail.com>
2017-02-13 17:32:11 -0700

    Add redirect on save edited reminder

--- app/components/edit-reminder.js
+++ app/components/edit-reminder.js
@@ -3,14 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   store: Ember.inject.service(),
 
-  
-
   actions: {
-    toggleEdit() {
-      this.get('store').findRecord('reminder', this.model.id).then(reminder => {
-        reminder.set('editable', true)
-      })
-    },
     updateReminder() {
       this.get('store').findRecord('reminder', this.model.id).then(reminder => {
         reminder.save()
