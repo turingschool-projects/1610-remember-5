9ad817da56704a604dc98f565818e0829108903c
mlimberg <limbergmike@gmail.com>
2017-02-13 17:12:15 -0700

    Update reminder details and save to store

--- app/components/individual-reminder.js
+++ app/components/individual-reminder.js
@@ -3,10 +3,17 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   store: Ember.inject.service(),
 
+  
+
   actions: {
     toggleEdit() {
-      this.get('store').findRecord('reminder', this.model.id).then((reminder) => {
+      this.get('store').findRecord('reminder', this.model.id).then(reminder => {
         reminder.set('editable', true)
+      })
+    },
+    updateReminder() {
+      this.get('store').findRecord('reminder', this.model.id).then(reminder => {
+        reminder.save()
       })
     }
   }
